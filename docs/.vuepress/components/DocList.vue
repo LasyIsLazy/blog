904a9122f048a79b5bcf2ba43e37a5e6c6b2759f
lasy <1255352506@qq.com>
2019-03-21 22:47:11 +0800

    增加 doc-list（文章列表）组件

---
+++ docs/.vuepress/components/DocList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="display: flex; flex-direction: column">
+    <div v-for="page of pages" :key="page.key" class="item">
+      <div class="date">{{ formateDate(page.frontmatter.date) }}</div>
+      <router-link :to="page.path" class="title">{{ page.title }}</router-link>
+      <div class="tags" style>{{ getTags(page) }}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'doc-list',
+  data() {
+    return {}
+  },
+  computed: {
+    category() {
+      const path = this.$page.path
+      const splitPath = typeof path !== undefined ? path.split('/') : ''
+      return splitPath ? splitPath[splitPath.length - 2] : 'posts' // 默认 posts
+    },
+    pages() {
+      return this.$site.pages.filter(item => item.title && item.path.split('/')[1] === this.category)
+    }
+  },
+  mounted() {
+    console.log(this)
+  },
+  methods: {
+    getTags(page) {
+      return page.frontmatter.tags && page.frontmatter.tags.join('，')
+    },
+    formateDate(dateStr) {
+      return dateStr && dateStr.replace(/\..+$/g, '').replace('T', ' ')
+    }
+  }
+}
+</script>
+<style scoped>
+.item {
+  padding: 20px 0;
+  margin-left: 20%;
+}
+.date {
+  color: #999;
+}
+.title {
+  font-size: 20px;
+}
+.tags {
+  color: #c2c5cd;
+  font-size: 12px;
+}
+</style>
--- docs/notes/README.md
+++ docs/notes/README.md
@@ -1,8 +1,1 @@
-<div style="display: flex; flex-direction: column">
-    <div v-for="page of $site.pages.filter(item => item.path !== '/' && item.title && item.path.split('/')[1] === 'notes')" :key="page.key" style="padding: 20px 0; margin-left: 25%; text-align: left;">
-        <router-link :to="page.path">
-            {{page.title}}
-            <div style="color: #c2c5cd; font-size: .5rem;">{{page.frontmatter.tags&&page.frontmatter.tags.join(',  ')}}</div>
-        </router-link>
-    </div>
-</div>
+<doc-list></doc-list>
