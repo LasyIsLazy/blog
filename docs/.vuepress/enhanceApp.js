1c0c473672643e6fc4cfccf7898a8d057ba2d846
lasy <1255352506@qq.com>
2019-03-26 12:47:23 +0800

    增加 FIXME

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,6 +5,8 @@ function initGitTalk(path) {
   if (splitPath[splitPath.length - 1] !== 'html') {
     return
   }
+  // FIXME: 编译时提示 document not defined
+  // 原因：(Vuepress 浏览器的 API 访问限制)[https://vuepress.vuejs.org/zh/guide/using-vue.html#%E6%B5%8F%E8%A7%88%E5%99%A8%E7%9A%84-api-%E8%AE%BF%E9%97%AE%E9%99%90%E5%88%B6]
   let gitalkContainer = document.getElementById('gitalk-container')
   if (!gitalkContainer) {
     gitalkContainer = document.createElement('div')
