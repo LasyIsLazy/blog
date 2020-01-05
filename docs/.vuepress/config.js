2183d8822c4e4c72ec3d08dc97586c0e51b6394b
Lasy <1255352506@qq.com>
2020-01-05 13:38:45 +0800

    reco主题的配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,74 @@
 module.exports = {
   title: 'Lasy',
   description: "Lasy's blog",
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1,user-scalable=no'
+      }
+    ]
+  ],
+  theme: 'reco',
   themeConfig: {
     nav: [
-      { text: '主页', link: '/' },
-      { text: '文章', link: '/posts/' },
-      // { text: '算法', link: '/algorithm/' },
-      // { text: '面试题', link: '/interview/' },
-      { text: 'GitHub', link: 'https://github.com/LasyIsLazy' }
+      {
+        text: 'Home',
+        link: '/',
+        icon: 'reco-home'
+      },
+      {
+        text: 'TimeLine',
+        link: '/timeline/',
+        icon: 'reco-date'
+      },
+      {
+        text: 'Contact',
+        icon: 'reco-message',
+        items: [
+          {
+            text: 'GitHub',
+            link: 'https://github.com/LasyIsLazy',
+            icon: 'reco-github'
+          }
+        ]
+      }
     ],
+    type: 'blog',
+    blogConfig: {
+      category: {
+        location: 2,
+        text: 'Category'
+      },
+      tag: {
+        location: 3,
+        text: 'Tag'
+      }
+    },
+    friendLink: [
+      {
+        title: '友情链接',
+        desc: '...',
+        email: '...@...com',
+        link: 'https://www.....com'
+      },
+    ],
+    logo: '/logo.jpeg',
+    search: true,
+    searchMaxSuggestions: 10,
     sidebar: 'auto',
-    sidebarDepth: 2,
-    lastUpdated: '最后更新时间'
+    lastUpdated: 'Last Updated',
+    author: '',
+    authorAvatar: '/logo.jpeg',
+    record: 'xxxx',
+    startYear: '2016'
   },
-  plugins: ['@vuepress/back-to-top'] 
-}
+};
