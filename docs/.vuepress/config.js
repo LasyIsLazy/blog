8ef45669e217a49231f2c362b1e8d1c4e050003a
Lasy <1255352506@qq.com>
2020-01-05 10:35:06 +0800

    删除笔记 Tab

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,6 @@ module.exports = {
       { text: '文章', link: '/posts/' },
       // { text: '算法', link: '/algorithm/' },
       // { text: '面试题', link: '/interview/' },
-      { text: '笔记', link: '/notes/' },
       { text: 'GitHub', link: 'https://github.com/LasyIsLazy' }
     ],
     sidebar: 'auto',
