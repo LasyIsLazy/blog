0f38470dd79ba7c1468eb0db855526c4ec5eb373
Lasy <1255352506@qq.com>
2023-01-13 16:09:21 +0800

    重构：升级reco主题

---
+++ .vuepress/config.ts
@@ -0,0 +1,108 @@
+import { defineUserConfig } from 'vuepress';
+import type { DefaultThemeOptions } from 'vuepress';
+import recoTheme from 'vuepress-theme-reco';
+
+export default defineUserConfig({
+  title: 'Lasy',
+  description: "Lasy's blog",
+  lang: 'zh-CN',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1,user-scalable=no',
+      },
+    ],
+  ],
+  theme: recoTheme({
+    style: '@vuepress-reco/style-default',
+    logo: '/logo.jpeg',
+    author: 'Lasy',
+    lastUpdatedText: '',
+    navbar: [
+      { text: '主页', link: '/' },
+      { text: '分类', link: 'categories/qianduan/1/' },
+      { text: '标签', link: '/tags/tag1/1/' },
+      // {
+      //   text: 'Docs',
+      //   children: [
+      //     { text: 'vuepress-reco', link: '/docs/theme-reco/theme' },
+      //     { text: 'vuepress-theme-reco', link: '/blogs/other/guide' },
+      //   ],
+      // },
+    ],
+    // bulletin: {
+    //   body: [
+    //     {
+    //       type: 'text',
+    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+    //       style: 'font-size: 12px;'
+    //     },
+    //     {
+    //       type: 'hr',
+    //     },
+    //     {
+    //       type: 'title',
+    //       content: 'QQ 群',
+    //     },
+    //     {
+    //       type: 'text',
+    //       content: `
+    //       <ul>
+    //         <li>QQ群1：1037296104</li>
+    //         <li>QQ群2：1061561395</li>
+    //         <li>QQ群3：962687802</li>
+    //       </ul>`,
+    //       style: 'font-size: 12px;'
+    //     },
+    //     {
+    //       type: 'hr',
+    //     },
+    //     {
+    //       type: 'title',
+    //       content: 'GitHub',
+    //     },
+    //     {
+    //       type: 'text',
+    //       content: `
+    //       <ul>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+    //       </ul>`,
+    //       style: 'font-size: 12px;'
+    //     },
+    //     {
+    //       type: 'hr',
+    //     },
+    //     {
+    //       type: 'buttongroup',
+    //       children: [
+    //         {
+    //           text: '打赏',
+    //           link: '/docs/others/donate.html'
+    //         }
+    //       ]
+    //     }
+    //   ],
+    // },
+    // valineConfig 配置与 1.x 一致
+    // valineConfig: {
+    //   appId: 'xxx',
+    //   appKey: 'xxx',
+    //   placeholder: '填写邮箱可以收到回复提醒哦！',
+    //   verify: true, // 验证码服务
+    //   // notify: true,
+    //   recordIP: true,
+    //   // hideComments: true // 隐藏评论
+    // },
+  }),
+  // debug: true,
+});
--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# 我的个人博客
+---
+home: true
+modules: # 指定首页展示模块
+  - Banner
+  - Blog
+  - Footer
+banner: # banner 模块的配置
+  heroText: Lasy
+  tagline: Bring yourself back to line.
+  heroImage: /logo.jpeg
+#   bgImage: /bg.png
+  bgImageStyle:
+    height: 450px
+blog: # blog 模块的配置
+  socialLinks: # 社交 icon 请到 [Xicons](https://www.xicons.org/#/zh-CN) 页面的 tabler 下获取，复制名称即可
+    - { icon: 'BrandGithub', link: 'https://github.com/LasyIsLazy' }
+footer: # 底部模块的配置
+  startYear: 2016
+date: 2023-01-13
+tags:
+- JavaScript
+categories: 
+ - 前端
+---
+# 博客
 
-博客地址：https://www.lasy.site
-
-github用来存放博客的文章，并与博客同步。
-
+博客地址：https://LasyIsLazy.github.io
